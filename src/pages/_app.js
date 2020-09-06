e33cd0ed22ec3cc539726366e9a8c6a15f19bed0
Carlos Montalvo <carlosmontalvo@zetanova.com>
2020-09-05 23:58:09 -0500

    Add persistent layout patterns

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import React from 'react'
+import App from 'next/app'
+import 'src/utils/theme/globals.css'
+import Layout, { siteTitle } from 'src/components/shared/Layout'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+//https://adamwathan.me/2019/10/17/persistent-layout-patterns-in-nextjs/
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
+  }
 }
 
 export default MyApp
